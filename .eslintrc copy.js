c908791fec1ade86c4096229aa41985cdd1fab26
Fernando Zuffo <fzuffo@gmail.com>
2019-10-02 00:51:41 -0400

    prettier/eslint + routesconfig

--- .eslintrc copy.js
+++ .eslintrc copy.js
@@ -1,12 +1,12 @@
 module.exports = {
   env: {
-    browser: true,
     es6: true,
   },
   extends: ['airbnb', 'prettier', 'prettier/react'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
+    __DEV__: 'readonly',
   },
   parser: 'babel-eslint',
   parserOptions: {
@@ -19,7 +19,12 @@ module.exports = {
   plugins: ['react', 'prettier'],
   rules: {
     'prettier/prettier': 'error',
-    'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
+    'react/jsx-filename-extension': [
+      'warn',
+      {
+        extensions: ['.jsx', '.js'],
+      },
+    ],
     'import/prefer-default-export': 'off',
   },
 };
